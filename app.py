e2496f24821f5d883e8483b842f1041706f0ec3d
Redha Moulla <151394880+RMoulla@users.noreply.github.com>
2024-11-28 06:05:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
 
-# Charger le modèle de régression logistique sauvegardé
+# Charger le modèle
 model = joblib.load('data/churn_model_clean.pkl')
 
 # Initialiser l'application Flask
 app = Flask(__name__)
 
+# Route principale pour afficher l'interface utilisateur
 @app.route('/', methods=['GET'])
 def home():
-    return "Application is running", 200
-# Définir la route principale pour la prédiction
+    return render_template('index.html')
+
+# Route pour gérer les prédictions
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Utiliser request.form pour extraire les données du formulaire HTML
-    age = float(request.form['Age'])
-    account_manager = int(request.form['Account_Manager'])
-    years = float(request.form['Years'])
-    num_sites = int(request.form['Num_Sites'])
+    try:
+        # Récupérer les données du formulaire
+        age = float(request.form['Age'])
+        account_manager = int(request.form['Account_Manager'])
+        years = float(request.form['Years'])
+        num_sites = int(request.form['Num_Sites'])
 
-    # Créer un tableau numpy pour les données de prédiction
-    features = np.array([[age, account_manager, years, num_sites]])
+        # Créer un tableau numpy pour les données
+        features = np.array([[age, account_manager, years, num_sites]])
 
-    # Effectuer la prédiction
-    prediction = model.predict(features)
-    print(prediction)
+        # Effectuer la prédiction
+        prediction = model.predict(features)
+        result = int(prediction[0])
 
-    # Convertir la prédiction en un format compréhensible
-    result = int(prediction[0])
+        # Retourner la réponse sous forme de JSON
+        return jsonify({'churn_prediction': result})
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
-    return jsonify({'churn_prediction': result})  # Le return doit être ici aussi dans une fonction
-
-# Fonction pour lancer le serveur Flask
+# Lancer l'application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5012, debug=True)
