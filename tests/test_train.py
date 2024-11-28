3674cb08ad749881356f4bc73e3168b91e655eb9
Redha Moulla <151394880+RMoulla@users.noreply.github.com>
2024-11-28 04:05:16 +0100

    Update test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,1 +1,9 @@
+import os
+import joblib
 
+def test_model_training():
+    # Vérifie si le modèle est correctement généré
+    os.system("python train.py")
+    assert os.path.exists("data/churn_model_clean.pkl")
+    model = joblib.load("data/churn_model_clean.pkl")
+    assert model is not None
